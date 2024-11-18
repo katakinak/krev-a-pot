996f71d07b7a9b2e073a36081bc00c8a096f5c4e
katakinak <kata.potemkina@gmail.com>
2024-11-18 16:49:37 +0100

    počítání

--- slova/slova/Program.cs
+++ slova/slova/Program.cs
@@ -1,6 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
 
-Console.WriteLine("Hello, World!");
-//dasjfafadlf
-Console.WriteLine(  "today");
+class TextAnalyzer : StreamReader;
+
+private Dictionary<string, int> slova;
+public int PocetSlov { get; set; }
+public int PocetZnakuBezMezer { get; set; }
+public int PocetZnakuSMezeama { get; set; }
+
+private TextAnalyzer (string soubor) : base(soubor);
+{
+    try
+
+    {
+        slova = new Dictionary<string, int>();
+        string obsah = ReadToEnd();
+
+        PocetZnakuSMezeama = obsah.Length;
+        PocetZnakuBezMezer = obsah.Count(znak => !char.bileznaky(znak));
+
+
+
+    }
+       
+}
