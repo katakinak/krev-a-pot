a3274049e113659c926f4c46cfc4562502703ef1
katakinak <kata.potemkina@gmail.com>
2024-11-25 16:34:48 +0100

    fungující soubor + milion chyb

---
+++ funju/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+class TextAnalyzer : StreamReader;
+{
+    private Dictionary<string, int> slova;
+    public int PocetSlov { get; set; }
+    public int PocetZnakuBezMezer { get; set; }
+    public int PocetZnakuSMezeama { get; set; }
+
+    private TextAnalyzer (string soubor) : base(soubor);
+    {
+        try
+
+        {
+            slova = new Dictionary<string, int>();
+            string obsah = ReadToEnd();
+
+            PocetZnakuSMezeama = obsah.Length;
+            PocetZnakuBezMezer = obsah.Count(znak => !char.bileznaky(znak));
+
+            var vsechnaslova = obsah.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            PocetSlov = vsechnaslova.Length;
+
+            foreach (var slovo in vsechnaslova)
+            {
+                string slovomale = slovo.ToLower;
+                if slova.ContainsKey(slovomale))
+                {
+                    slova[slovomale]++;
+                }
+
+                else
+                {
+                    slova[slovomale] = 1;
+                }
+
+            }
+
+
+
+        }
+        catch { FileNotFoundException}
+        {
+            Console.WriteLine($"soubor {'soubor'} neexistuje");
+        }
+    
+                catch { Exception ex}
+        {
+            Console.WriteLine($"chyba při zpracování souboru: {ex.Message}");
+        }
+    }
+    public Dictionary<string, int> ziskatfrekvencislov;
+    {
+        return new Dictionary<string, int>(slova);
+    }
+    
+}
---
+++ funju/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("funju")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GCHD")]
+[assembly: AssemblyProduct("funju")]
+[assembly: AssemblyCopyright("Copyright © GCHD 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("74db6c66-3d9c-4cc6-9485-5373ad679206")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
