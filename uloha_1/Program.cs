3bfce753ae886602c4f7670311c4354f605ae45c
katakinak <kata.potemkina@gmail.com>
2024-12-16 16:50:34 +0100

    some

--- uloha_1/Program.cs
+++ uloha_1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,33 +17,74 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static string[,] Vytvoritpole()
-    { 
-        string[,] pole = new string[4, 4];
+    static string[,] Vytvoritpole(int velikost)
+    {
+        string[,] pole = new string[velikost, velikost];
 
-        for (int i = 0; i< 4; i++)
+        for (int i = 0; i < 4; i++)
         {
-            for (int j = 0; j< 4; j++)
+            for (int j = 0; j < 4; j++)
             {
                 pole[i, j] = "cesta";
             }
         }
         return pole;
-        
+
     }
     static string Vypispole(string[,] pole)
     {
         string vypis = " ";
-        for (int i = 0;i< pole.GetLength(0); i++)
+        for (int i = 0; i < pole.GetLength(0); i++)
         {
             for (int j = 0; j < pole.GetLength(1); j++)
             {
                 vypis += pole[i, j] + "\t";
             }
-            vypis += "\n";    
+            vypis += "\n";
         }
         return vypis;
     }
+    //BFS
+    static (int, List<(int, int)>) najdicestu(
+        int n,
+        List<(int x, int y, int t)> laviny,
+        List<(int dx, int dy)> tahy
+) 
+{
+    
+    Queue<(int x, int y, int time, List<(int, int)> Path)> fronta
+        = new Queue<(int, int, int, List<(int, int)>)>();
+
+
+    fronta.Enqueue((0,0,0, new List<(int, int)> {(0,0)}));
+
+    HashSet<(int, int, int)> navstiveno
+        = new HashSet<(int, int, int)>();
+
+    while (fronta.Count > 0)
+    { 
+        var (x, y, time, path) = fronta.Dequeue();
+
+        if (x == n - 1 && y == n - 1)
+        {
+            return (time, path);
+        }
+            bool jeblokovana = laviny.Any(
+                lavina => lavina.x == x && lavina.y == y && lavina.t <= time
+                );
+            if (jeblokovana)
+            {
+                continue;
+            }
+            if (navstiveno.Contains(x, y, time))
+            {
+                continue ;
+            }
+    }
+
+
+
+
     static void Main()
     {
         string[,]polehry = Vytvoritpole();
